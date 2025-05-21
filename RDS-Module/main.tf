dd68aa229777ddfb5274e45164fe3f7595b4d676
pipinokyo <cuneyt.omer@gmail.com>
2025-05-21 01:06:27 -0400

    explanations

--- RDS-Module/main.tf
+++ RDS-Module/main.tf
@@ -1,22 +1,22 @@
-resource "aws_db_instance" "this" {
-  identifier           = "${var.tags["Name"]}-${var.service_name}"
-  engine              = "mysql"
-  engine_version      = "5.7"
-  instance_class      = var.instance_class
-  allocated_storage   = 20
-  storage_type        = "gp2"
+resource "aws_db_instance" "this" {                                   # RDS instance resource
+  identifier           = "${var.tags["Name"]}-${var.service_name}"    # Instance identifier, combining the Name tag and service name. in this case, it is "wordpress-rds"
+  engine              = "mysql"                                       # Database engine type (MySQL in this case).
+  engine_version      = "5.7"                                         # Version of the database engine.
+  instance_class      = var.instance_class                            # Instance class (e.g., db.t3.micro). 
+  allocated_storage   = 20                                            # Allocated storage size in GiB.
+  storage_type        = "gp2"                                         # Storage type (General Purpose SSD).
+
+  db_name             = var.db_name                                   # Database name.
+  username            = var.db_username                               # Database username.
+  password            = var.db_password                               # Database password.
   
-  db_name             = var.db_name
-  username            = var.db_username
-  password            = var.db_password
+  db_subnet_group_name   = var.db_subnet_group                        # Subnet group for the database instance.
+  vpc_security_group_ids = [var.security_group_id]                    # Security group IDs to associate with the instance.
   
-  db_subnet_group_name   = var.db_subnet_group
-  vpc_security_group_ids = [var.security_group_id]
+  skip_final_snapshot    = true                                       # Skip final snapshot on deletion. 
+  publicly_accessible    = true                                       # Make the instance publicly accessible. 
   
-  skip_final_snapshot    = true
-  publicly_accessible    = true
-  
-  tags = merge(var.tags, {
-    Name = "${var.tags["Name"]}-${var.service_name}"
+  tags = merge(var.tags, {                                            # Merge the tags passed to the module with the instance-specific tags
+    Name = "${var.tags["Name"]}-${var.service_name}"                  # The name of the instance, which is a combination of the Name tag and the service name. in this case, it is "wordpress-rds"
   })
 } 
--- SG-Module/main.tf
+++ SG-Module/main.tf
@@ -1,53 +1,53 @@
-resource "aws_security_group" "ec2" {
-  name        = "${var.tags["Name"]}-${var.service_name}-ec2"
-  description = "Security group for EC2 instance"
-  vpc_id      = var.vpc_id
+resource "aws_security_group" "ec2" {                             # Security group for EC2 instance
+  name        = "${var.tags["Name"]}-${var.service_name}-ec2"     # Name of the security group.
+  description = "Security group for EC2 instance"                 # Description of the security group. 
+  vpc_id      = var.vpc_id                                        # VPC ID where the security group will be created.  
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress {                                                       # Ingress rule for EC2 instance
+    from_port   = 80                                              # Port for HTTP (80).
+    to_port     = 80                                              # Port for HTTP (80).  
+    protocol    = "tcp"                                           # Protocol (TCP).
+    cidr_blocks = ["0.0.0.0/0"]                                   # Allow traffic from anywhere.
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress {                                                       # Ingress rule for SSH access
+    from_port   = 22                                              # Port for SSH (22).
+    to_port     = 22                                              # Port for SSH (22).  
+    protocol    = "tcp"                                           # Protocol (TCP).
+    cidr_blocks = ["0.0.0.0/0"]                                   # Allow traffic from anywhere.
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  egress {                                                        # Egress rule for all outbound traffic
+    from_port   = 0                                               # Allow all outbound traffic.
+    to_port     = 0                                               # Allow all outbound traffic.
+    protocol    = "-1"                                            # All protocols.
+    cidr_blocks = ["0.0.0.0/0"]                                   # Allow traffic to anywhere. 
   }
 
-  tags = merge(
-    var.tags,
+  tags = merge(                                                   # Tags for the security group 
+    var.tags,                                                     # Merge the tags passed to the module with the security group-specific tags 
     {
-      Name = "${var.tags["Name"]}-${var.service_name}-ec2"
+      Name = "${var.tags["Name"]}-${var.service_name}-ec2"        # The name of the security group, which is a combination of the Name tag and the service name. 
     }
   )
 }
 
-resource "aws_security_group" "rds" {
-  name        = "${var.tags["Name"]}-${var.service_name}-rds"
-  description = "Security group for RDS instance"
-  vpc_id      = var.vpc_id
+resource "aws_security_group" "rds" {                              # Security group for RDS instance  
+  name        = "${var.tags["Name"]}-${var.service_name}-rds"      # Name of the security group.  
+  description = "Security group for RDS instance"                  # Description of the security group. 
+  vpc_id      = var.vpc_id                                         # VPC ID where the security group will be created.
 
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2.id]
+  ingress {                                                        # Ingress rule for RDS instance                  
+    from_port       = 3306                                         # Port for MySQL (3306).
+    to_port         = 3306                                         # Port for MySQL (3306).
+    protocol        = "tcp"                                        # Protocol (TCP).
+    security_groups = [aws_security_group.ec2.id]                  # Security group ID for EC2 instance.
   }
 
-  tags = merge(
-    var.tags,
+  tags = merge(                                                    # Tags for the security group
+    var.tags,                                                      # Merge the tags passed to the module with the security group-specific tags 
     {
-      Name = "${var.tags["Name"]}-${var.service_name}-rds"
+      Name = "${var.tags["Name"]}-${var.service_name}-rds"         # The name of the security group, which is a combination of the Name tag and the service name.
     }
   )
 } 
